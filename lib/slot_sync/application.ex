f485d7105e3831d38a88f2c571b8d9a04152a415
Ian Vaughan <ian@quiqup.com>
2018-10-05 10:46:27 +0100

    Initial supervisor

--- lib/slot_sync/application.ex
+++ lib/slot_sync/application.ex
@@ -1,20 +1,24 @@
 defmodule SlotSync.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
-
   use Application
 
-  def start(_type, _args) do
-    # List all child processes to be supervised
-    children = [
-      # Starts a worker by calling: SlotSync.Worker.start_link(arg)
-      # {SlotSync.Worker, arg},
-    ]
+  import Supervisor.Spec
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
+  def start(_type, _args) do
     opts = [strategy: :one_for_one, name: SlotSync.Supervisor]
-    Supervisor.start_link(children, opts)
+
+    app = Supervisor.start_link(children(), opts)
+
+  defp children do
+    with conf <- Map.new(config()) do
+      [:shift_processor] |> Enum.flat_map(&children(&1, conf))
+    end
+  end
+
+
+
+  defp children(:shift_processor, _) do
+    [
+      worker(SlotSync.Processor.Run, [])
+    ]
   end
 end
