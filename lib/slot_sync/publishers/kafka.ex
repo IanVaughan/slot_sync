372be4bf3240730a93c600a4fc6a6b924c8d9e81
Ian Vaughan <ian@quiqup.com>
2018-10-12 15:35:34 +0100

    Parse dates

--- lib/slot_sync/publishers/kafka.ex
+++ lib/slot_sync/publishers/kafka.ex
@@ -43,15 +43,15 @@ defmodule SlotSync.Publishers.Kafka do
     [
       {"account_id", event["account_id"]},
       {"acknowledged", event["acknowledged"]},
-      {"acknowledged_at", string_or_nil(event["acknowledged_at"])},
+      {"acknowledged_at", event["acknowledged_at"] |> string_or_nil() |> date_to_iso()},
       {"actionable", event["actionable"]},
       {"alerted", event["alerted"]},
       {"block_id", event["block_id"]},
       {"break_time", event["break_time"]},
       {"color", event["color"]},
-      {"created_at", event["created_at"]},
+      {"created_at", event["created_at"] |> date_to_iso()},
       {"creator_id", event["creator_id"]},
-      {"end_time", event["end_time"]},
+      {"end_time", event["end_time"] |> date_to_iso()},
       {"id", event["id"]},
       {"instances", event["instances"]},
       {"is_open", event["is_open"]},
@@ -59,20 +59,30 @@ defmodule SlotSync.Publishers.Kafka do
       {"linked_users", :null},
       {"location_id", event["location_id"]},
       {"notes", event["notes"]},
-      {"notified_at", string_or_nil(event["notified_at"])},
+      {"notified_at", event["notified_at"] |> string_or_nil() |> date_to_iso()},
       {"position_id", event["position_id"]},
       {"published", event["published"]},
-      {"published_date", event["published_date"]},
+      {"published_date", event["published_date"] |> date_to_iso()},
       {"shiftchain_key", event["shiftchain_key"]},
       {"site_id", event["site_id"]},
-      {"start_time", event["start_time"]},
-      {"updated_at", event["updated_at"]},
+      {"start_time", event["start_time"] |> date_to_iso()},
+      {"updated_at", event["updated_at"] |> date_to_iso()},
       {"user_id", event["user_id"]}
     ]
   end
 
   defp string_or_nil(_), do: {"null", :null}
   defp string_or_nil(value), do: {"string", value}
+
+  # WIW dates are formatted as "Wed, 26 Sep 2018 12:30:28 +0100"
+  # But we want iso8601, eg: "2018-09-26T12:30:28+01:00"
+  defp date_to_iso(nil), do: nil
+
+  defp date_to_iso(date_string) do
+    with {:ok, time} <- Timex.parse(date_string, "{RFC1123}") do
+      DateTime.to_iso8601(time)
+    end
+  end
 
   defp build_request({:error, error}, event) do
     stats("publisher.encoding.error")
